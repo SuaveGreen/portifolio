b70eed836092b292e0041d57833bb7431758a7d1
Pedro Caetano <pedro.kaetano2003@gmail.com>
2024-01-25 18:50:13 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
 import Title from '../../components/Title'
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
+
+import { Descricao, ThemeButton, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/suavegreen.png" />
-    <Title fontSize={20}>Pedro Caetano</Title>
+    <SidebarContainer>
+      <Avatar />
+      <Title fontSize={20}>Pedro Caetano</Title>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        suavegreen
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Full Stack Python
+      </Descricao>
+      <ThemeButton>Trocar Tema</ThemeButton>
+    </SidebarContainer>
   </aside>
 )
 
